65f6994927fa74e8add43bc404c8db18936c67b7
pdjain-9147 <128225820+pdjain-9147@users.noreply.github.com>
2023-04-19 16:19:06 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -1,28 +1,22 @@
-        let string ="";
-        let buttons = document.querySelectorAll('.button');
-        Array.from(buttons).forEach((button)=>{
-            button.addEventListener('click', (e) => {
+let string ="";
+let buttons = document.querySelectorAll('.button');
+Array.from(button).forEach(button)=>{
+        button.addEventListener('click', (e) => {
                 if(e.target.innerHTML =='DEL'){
-                    string = string.toString().slice(0,-1)
-                    document.querySelector('input').value =string;
+                        string = string.toString().slice(0,-1)
+                        document.querySelector('input').value =string;
                 }
                 else if(e.target.innerHTML == 'AC'){
-                    string = "";
-                    document.querySelector('input').value=string;
-                }
-
-                else if(e.target.innerHTML == '='){
-                    string = eval(string);
-                    document.querySelector('input').value=string;
-
+                        string = "";
+                        document.querySelector('input').value=string;
                 }
                 else{
-
-                    console.log(e.target)
-                    string = string + e.target.innerHTML;
-                    document.querySelector('input').value=string;
+                        console.log(e.target)
+                        string = string + e.target.innerHTML;
+                        document.querySelector('input').value=string;
                 }
-            })
-
         })
+})
+                
+        
 
